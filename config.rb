d470b43f1309b92485097702c42651d6fad8acd0
Samuel Lie <>
2018-11-11 16:42:03 +0100

    Add deploy and minifying

--- config.rb
+++ config.rb
@@ -11,6 +11,11 @@ activate :blog do |blog|
   blog.layout = "case-layout"
 end
 
+activate :directory_indexes
+
+activate :relative_assets
+set :relative_links, true
+
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
@@ -46,7 +51,17 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  deploy.build_before = true
+end
+
+configure :build do
+  activate :minify_css
+  activate :minify_html
+end
